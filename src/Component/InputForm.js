c2b6c99dd6386f228bc9d027205dc4e3292e370a
DuaaH <dodohalaiqa.1@gmail.com>
2019-08-22 11:25:44 +0300

    make form Component as function

--- src/Component/InputForm.js
+++ src/Component/InputForm.js
@@ -1,23 +1,27 @@
-import React, { Component } from "react"
-import '../style.css'
+import React from "react"
+import "../style.css"
 
-class InputForm extends Component {
-    render() {
-        return (
-            < div >
-                <img src='https://jipanel.com/blog/image/19/post-1529640873-image_fileuser_id_43.png' alt="Logo" />
-                <h1>To Do App</h1>
-                <form className="taskForm">
-                    <input className="box-input" onChange={this.props.handleChange} type="text" value={this.props.todo} placeholder="Add Task..." />
-                    <button className="addBtn" onClick={this.props.AddItem}>ADD</button>
-                </form>
-            </div >
-        )
-    }
+const InputForm = ({ handleChange, todo, AddItem }) => {
+  return (
+    <div>
+      <img
+        src="https://jipanel.com/blog/image/19/post-1529640873-image_fileuser_id_43.png"
+        alt="Logo"
+      />
+      <h1>To Do App</h1>
+      <form className="taskForm">
+        <input
+          className="box-input"
+          onChange={handleChange}
+          type="text"
+          value={todo}
+          placeholder="Add Task..."
+        />
+        <button className="addBtn" onClick={AddItem}>
+          ADD
+        </button>
+      </form>
+    </div>
+  )
 }
-
-
-
-
-
 export default InputForm
